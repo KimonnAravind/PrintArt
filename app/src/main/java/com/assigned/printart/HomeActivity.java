a32ad36e95d8630ee22afcf635cb028d9368f910
KimonnAravind <aravindselvam03@gmail.com>
2020-02-24 18:29:06 +0530

    git commit -m "PrintArt2/24/2020"

--- app/src/main/java/com/assigned/printart/HomeActivity.java
+++ app/src/main/java/com/assigned/printart/HomeActivity.java
@@ -300,6 +300,7 @@ public class HomeActivity extends AppCompatActivity implements  FirebaseViewer,N
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
+
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
