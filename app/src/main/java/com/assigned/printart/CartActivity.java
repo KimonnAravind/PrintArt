18c6e0bf399f6ce19d5f5b36200e745b4b1f8195
KimonnAravind <aravindselvam03@gmail.com>
2020-03-06 12:38:47 +0530

    git commit -m "PrintArt3/06/2020"

--- app/src/main/java/com/assigned/printart/CartActivity.java
+++ app/src/main/java/com/assigned/printart/CartActivity.java
@@ -10,31 +10,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.assigned.printart.Model.DisplayProducts;
+import com.assigned.printart.Paper.PaperStore;
 import com.assigned.printart.Viewer.DisplayProductViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import io.paperdb.Paper;
 
 public class CartActivity extends AppCompatActivity
 {
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
-    DatabaseReference databaseReference;
+    private TextView deliveryAdd;
+    DatabaseReference databaseReference,AddressReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
         recyclerView=(RecyclerView)findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
-
+        deliveryAdd=(TextView)findViewById(R.id.deliveryA);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+        String users= Paper.book().read(PaperStore.UserLoginID);
         databaseReference= FirebaseDatabase.getInstance().getReference().child("UserCart")
-                .child("7904168617");
+                .child(users);
+        AddressReference= FirebaseDatabase.getInstance().getReference().child("EndUsers").child(users);
         FirebaseRecyclerOptions<DisplayProducts> options=
                 new FirebaseRecyclerOptions.Builder<DisplayProducts>().setQuery(databaseReference, DisplayProducts.class)
                         .build();
@@ -62,5 +73,30 @@ public class CartActivity extends AppCompatActivity
         };
         recyclerView.setAdapter(adapter);
         adapter.startListening();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        AddressReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child("Address").exists())
+                {
+
+
+                deliveryAdd.setText("Deliver to: "+dataSnapshot.child("Address").getValue().toString());
+                }
+                else
+                {
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
